4b93916df492a7e20aa70e25e746a692188a582e
Santiago <santiagomintegui@gmail.com>
2023-08-25 20:29:58 -0300

    change some styles

--- client/src/app/registro/Form.tsx
+++ client/src/app/registro/Form.tsx
@@ -4,12 +4,12 @@ import Input from "@/components/common/Input";
 export default function RegisterForm() {
     return (
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm" id="register-form">
-                <form className="space-y-6" action="#" method="POST">
+                <form className="grid grid-rows-5 gap-5" action="#" method="POST">
                     <Input type="text" id="name" name="name" label="Nombre" placeholder="Nicolas Rodriguez" />
                     <Input type="email" id="email" name="email" label="Email" placeholder="nico22@fing.edu.uy"/>
                     <Input type="password" id="password" name="password" label="Contraseña" placeholder="contraseña"/>
                     <Input type="date" id="date" name="date" label="Fecha de nacimiento" />
-                    <Button type="submit" text="Crear Cuenta" />
+                    <Button type="submit" text="Crear Cuenta" className="mt-5"/>
                 </form>
             </div>
     )
--- client/src/app/registro/View.tsx
+++ client/src/app/registro/View.tsx
@@ -3,13 +3,15 @@ import RegisterForm from "./Form";
 
 export default function Form() {
     return (
-        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" id="register-view">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm" id="register-header">
+        <div className="grid grid-row-3 min-h-full justify-center px-6 py-12 lg:px-8" id="register-view">
+            <div id="register-header">
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Registrarse</h2>
                 <p className="mt-4 text-center tracking-tight text-gray-900">Crea tu cuenta para empezar a buscar grupos de estudio</p>
             </div>
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm" id="register-body">
+            <div id="register-body">
                 <RegisterForm />
+            </div>
+            <div>
                 <p className="mt-10 text-center text-sm text-gray-500">
                 Ya estas registrado?
                 <Link href="/login" className="font-semibold leading-6 text-blue-600 hover:text-blue-500"> Login</Link>
