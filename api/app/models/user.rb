d61f03d97ba158a9304bceb85da3dfab73c80996
Juan Arias <juan.arias@cedarcode.com>
2023-08-25 20:19:50 -0300

    Add model validations and specs

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :name, :birth_date, presence: true
 end
--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build :user }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:birth_date) }
+  end
 end
