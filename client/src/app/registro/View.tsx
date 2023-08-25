b71ad28fdc64d72192554e1e18443c8340b75bc8
Santiago <santiagomintegui@gmail.com>
2023-08-25 20:59:57 -0300

    add finder.com title to mobile

--- client/src/app/registro/View.tsx
+++ client/src/app/registro/View.tsx
@@ -5,6 +5,7 @@ export default function Form() {
     return (
         <div className="grid grid-row-3 min-h-full justify-center px-6 py-12 lg:px-8" id="register-view">
             <div id="register-header">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 block md:hidden">finder.com</h2>
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Registrarse</h2>
                 <p className="mt-4 text-center tracking-tight text-gray-900">Crea tu cuenta para empezar a buscar grupos de estudio</p>
             </div>
