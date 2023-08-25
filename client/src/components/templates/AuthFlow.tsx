ec7b35e2b37a68c86c1c697ec88bc6cdb170b6e4
Santiago <santiagomintegui@gmail.com>
2023-08-25 20:00:58 -0300

    fix container style

--- client/src/components/templates/AuthFlow.tsx
+++ client/src/components/templates/AuthFlow.tsx
@@ -8,9 +8,9 @@ type AuthFlowProps = {
 export default function AuthFlow({children}:AuthFlowProps) {
     return (
         <div className="h-screen flex items-center justify-center bg-gray-100 overflow-y-hidden" id="container">
-            <div className="max-w-5xl bg-white shadow-lg rounded-lg flex items-stretch h-full sm:h-auto" id="image-container">
+            <div className="max-w-5xl bg-white shadow-lg rounded-lg flex items-stretch h-full sm:h-auto" id="flex-container">
                 {/* Left side (Image) - hidden on mobile */}
-                <div className="hidden md:flex w-1/2 justify-center items-center relative overflow-hidden flex-shrink-0">
+                <div className="hidden md:flex w-1/2 justify-center items-center relative overflow-hidden flex-shrink-0" id="image-container">
                     <Image 
                         src="https://media.istockphoto.com/id/639359406/es/foto/estudiantes-que-estudian-en-la-biblioteca-de-la-universidad.jpg?s=2048x2048&w=is&k=20&c=BNaCCgf0UTSW2GRT4tCQEtuSfdAfZynDGwQ2uDR9qvo=" 
                         alt="Grupo de estudiantes" 
@@ -20,7 +20,7 @@ export default function AuthFlow({children}:AuthFlowProps) {
                         />
                 </div>
                 {/* Right side (Form Component) */}
-                <div className="w-full md:w-5/12 md:pl-6 flex-shrink-0" id="children-container">
+                <div className="w-full md:w-1/2" id="children-container">
                 {children}
                 </div>
             </div>
