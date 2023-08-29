22f92c9ebbde2a7cc95122fa38e75761051730ec
Juan Arias <juan.arias@cedarcode.com>
2023-08-29 20:18:48 -0300

    feat(User): Add email format validation
      * This will only allow users with `@fing.edu.uy` domain to be created.

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { build :user }
-
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:birth_date) }
+
+    describe 'email' do
+      let(:subject) { build :user, email: }
+
+      context 'invalid format' do
+        let(:email) { 'test@mail.com' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'valid format' do
+        let(:email) { 'test@fing.edu.uy' }
+
+        it { is_expected.to be_valid }
+      end
+    end
   end
 end
