6b4a30d54a8a0710ad98c529fa16d69699809bad
marcelog@qualabs.com <mgodoydeolivera@gmail.com>
2023-09-02 19:33:21 -0300

    improve styling

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       gridTemplateRows: {
-        'register-form': '70px 70px 70px 70px 70px 80px',
+        'register-form': '70px 70px 70px 70px 80px',
       },
     },
   },
