032129130161d8fb4fbf8c1d5a2b31cbf6785bc4
andresconti <77336573+andresconti@users.noreply.github.com>
2023-09-05 20:36:07 -0300

    fix merge

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :validatable, :jwt_authenticatable,
+         :rememberable, :validatable, :confirmable, :jwt_authenticatable,
          jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
   # Validations
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
+    confirmations: 'confirmations'
   }
 end
