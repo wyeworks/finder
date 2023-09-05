de55f200c0a2367aecacf31e77c55f2d19d3e25a
Santiago <santiagomintegui@gmail.com>
2023-09-05 12:55:49 -0300

    create next-auth file

---
+++ client/src/app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,53 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const handler = NextAuth({
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const URL = process.env.RAILS_API_URL + '/users/login';
+        const res = await fetch(URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+        const user = await res.json();
+        if (res.ok && user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) token = user as unknown as { [key: string]: any };
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user = { ...token };
+      return session;
+    },
+  },
+  secret: 'supersecret',
+  pages: {
+    signIn: '/login',
+  },
+});
+
+export { handler as GET, handler as POST };
