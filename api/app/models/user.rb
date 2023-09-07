eab67954e6b3e8334d747a62472ef36848c34b9a
nodescanses <ivngtz11@gmail.com>
2023-09-06 21:27:36 -0300

    fix on update bio and password

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   # Validations
   validates :name, presence: true
-  validate :password_complexity
+  validate :password_complexity, if: :password_present?
 
   private
 
@@ -15,5 +15,9 @@ class User < ApplicationRecord
 
     errors.add :password, 'Complexity requirement not met. ' \
                           'Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  def password_present?
+    password.present?
   end
 end
