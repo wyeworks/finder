1ec7d29b63146bba8bf2ebd52e364e0f549ccf5a
Andres <christianconti95@gmail.com>
2023-09-08 08:58:09 -0300

    fix rubocop offenses

--- api/app/controllers/users/confirmations_controller.rb
+++ api/app/controllers/users/confirmations_controller.rb
@@ -1,10 +1,11 @@
 module Users
   class ConfirmationsController < Devise::ConfirmationsController
     private
-    def after_confirmation_path_for(resource_name, resource)
-      #new_session_path(resource_name)
-      #Rails.logger.debug "Estoy en after_confirmation_path_for"
-      #Código para redirigir luego de confirmar"
+
+    def after_confirmation_path_for
+      # new_session_path(resource_name)
+      # Rails.logger.debug "Estoy en after_confirmation_path_for"
+      # Código para redirigir luego de confirmar"
       'https://www.google.com'
     end
   end
