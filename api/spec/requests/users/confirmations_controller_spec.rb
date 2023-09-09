0fd7c340c132f04636d1c0eabab107d05e383e15
Andres <christianconti95@gmail.com>
2023-09-09 19:28:05 -0300

    test improved

--- api/spec/requests/users/confirmations_controller_spec.rb
+++ api/spec/requests/users/confirmations_controller_spec.rb
@@ -5,17 +5,29 @@ RSpec.describe Users::ConfirmationsController, type: :request do
     let(:user) { create(:user, :with_confirmation_token) }
 
     context 'when the confirmation token is valid' do
-      it 'confirms the user and updates confirmed_at' do
+      before do
         get user_confirmation_path(confirmation_token: user.confirmation_token)
+      end
 
+      it 'returns http success' do
+        expect(response).to have_http_status(:found)
+      end
+
+      it 'confirms the user and updates confirmed_at' do
         expect(user.reload.confirmed_at).not_to be_nil
       end
     end
 
     context 'when the confirmation token is invalid' do
-      it 'does not confirm the user' do
+      before do
         get user_confirmation_path(confirmation_token: 'invalid_token')
+      end
 
+      it 'returns http success' do
+        expect(response).to have_http_status(:found)
+      end
+      
+      it 'does not confirm the user' do
         expect(user.reload.confirmed_at).to be_nil
       end
     end
