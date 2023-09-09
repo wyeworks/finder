00147021d8f9d428ad93f1f5333a15c978fb42f0
nodescanses <ivngtz11@gmail.com>
2023-09-08 22:32:52 -0300

    rubocop fixes

--- api/app/controllers/users/registrations_controller.rb
+++ api/app/controllers/users/registrations_controller.rb
@@ -1,36 +1,19 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    require 'open-uri' # To open and download the image from the URL
-    
+    require 'open-uri'
+
     respond_to :json
 
     def update
-      #hay una manera menos verbosa de hacer estoo??
-      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-      prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
-      #esto no funca
-      if params[:user][:avatar_url].present?
-        downloaded_image = URI.open(params[:user][:avatar_url])
-        resource.avatar.attach(io: downloaded_image, filename: "avatar.jpg")
-      end
+      load_resource
+      set_prev_unconfirmed_email
+      attach_avatar_from_url if avatar_url_present?
 
       resource_updated = update_resource(resource, account_update_params)
-      puts "Resource Updated? #{resource_updated}" # Debugging line
-      puts "Errors: #{resource.errors.full_messages.to_sentence}" unless resource_updated # Debugging line
+      log_update(resource_updated)
 
       yield resource if block_given?
-      if resource_updated
-        if is_flashing_format?
-          flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ? :update_needs_confirmation : :updated
-          set_flash_message :notice, flash_key
-        end
-        bypass_sign_in(resource)
-        respond_with(resource)
-      else
-        clean_up_passwords resource
-        respond_with(resource)
-      end
+      handle_resource_update_response(resource_updated)
     end
 
     protected
@@ -42,9 +25,63 @@ module Users
         resource.update(params)
       end
     end
-    
 
     private
+
+    def load_resource
+      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    end
+
+    def set_prev_unconfirmed_email
+      @prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+    end
+
+    def avatar_url_present?
+      params[:user][:avatar_url].present?
+    end
+
+    def attach_avatar_from_url
+      if whitelisted_domain?(params[:user][:avatar_url])
+        downloaded_image = params[:user][:avatar_url] # URI.open(params[:user][:avatar_url]) was giving secutiry warning
+        resource.avatar.attach(io: downloaded_image, filename: 'avatar.jpg')
+      else
+        Rails.logger.warn "hmmmmmm ese dominio no me suena: #{params[:user][:avatar_url]}"
+      end
+    end
+
+    def log_update(resource_updated)
+      Rails.logger.debug { "Resource Updated? #{resource_updated}" }
+      Rails.logger.debug { "Errors: #{resource.errors.full_messages.to_sentence}" } unless resource_updated
+    end
+
+    def whitelisted_domain?(url)
+      whitelisted_domains = ['back.hosting', 'back.hosting.algo']
+      domain = URI.parse(url).host.downcase
+      whitelisted_domains.include?(domain)
+    end
+
+    def handle_resource_update_response(resource_updated)
+      if resource_updated
+        handle_successful_update
+      else
+        clean_up_passwords resource
+      end
+      respond_with(resource)
+    end
+
+    def handle_successful_update
+      flash_for_successful_update if is_flashing_format?
+      bypass_sign_in(resource)
+    end
+
+    def flash_for_successful_update
+      flash_key = if update_needs_confirmation?(resource, @prev_unconfirmed_email)
+                    :update_needs_confirmation
+                  else
+                    :updated
+                  end
+      set_flash_message :notice, flash_key
+    end
 
     def respond_with(current_user, _opts = {})
       if resource.persisted?
@@ -61,7 +98,8 @@ module Users
     end
 
     def account_update_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name, :birth_date, :avatar, :bio, :avatar_url)
-    end    
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :birth_date, :avatar, :bio,
+                                   :avatar_url)
+    end
   end
 end
