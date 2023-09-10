738b015870dceec7a88d41acd73bf0b74a83a784
Andres <christianconti95@gmail.com>
2023-09-10 19:22:04 -0300

    mailer configs

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
