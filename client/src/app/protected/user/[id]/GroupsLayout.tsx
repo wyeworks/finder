b54c2a5500919899a0ec2b74c2abdbc2aad39ee4
Frodo2412 <brunolemus553@gmail.com>
2023-09-10 18:56:48 -0300

    feat: Groups are responsive

--- client/src/app/protected/user/[id]/GroupsLayout.tsx
+++ client/src/app/protected/user/[id]/GroupsLayout.tsx
@@ -9,7 +9,7 @@ function GroupCard(props: { group: StudyGroup }) {
   return (
     <div
       className={
-        'm-10 flex w-[320px] flex-col overflow-hidden rounded-2xl shadow-2xl md:w-[444px]'
+        'm-10 flex w-[444px] flex-col overflow-hidden rounded-2xl shadow-2xl'
       }
     >
       <Image
@@ -23,8 +23,12 @@ function GroupCard(props: { group: StudyGroup }) {
         <h1 className={'text-base font-bold text-[#242760]'}>
           {props.group.subject}
         </h1>
-        <h1>{props.group.name}</h1>
-        <p>{props.group.description}</p>
+        <h1 className={'text-xl font-normal text-[#050838]'}>
+          {props.group.name}
+        </h1>
+        <p className={'text-base font-light text-[#A0A0A0]'}>
+          {props.group.description}
+        </p>
       </div>
     </div>
   );
@@ -42,9 +46,11 @@ export default function GroupsLayout(props: GroupsLayoutProps) {
           'p-5 text-[#2B2D54] md:border-b-2 md:bg-[#2B2D54] md:text-white'
         }
       >
-        <h1 className={'text-2xl font-extrabold md:text-4xl'}>Grupos</h1>
+        <h1 className={'text-4xl font-extrabold'}>Grupos</h1>
       </div>
-      <div className={'flex flex-row flex-wrap'}>
+      <div
+        className={'flex flex-row flex-wrap justify-center md:justify-start'}
+      >
         {props.groups.map((group) => (
           <GroupCard key={group.name} group={group} />
         ))}
