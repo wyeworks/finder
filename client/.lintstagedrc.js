ba1669e578307404a5e0481d66707c76221496ff
Frodo2412 <brunolemus553@gmail.com>
2023-09-11 16:12:29 -0300

    fix: re enable eslint

--- client/.lintstagedrc.js
+++ client/.lintstagedrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  // TODO: Arreglar despues
-  // // this will check Typescript files
-  // '**/*.(ts|tsx)': () => 'tsc --noEmit',
+  // this will check Typescript files
+  '**/*.(ts|tsx)': () => 'tsc --noEmit',
 
   // This will lint and format TypeScript and                                             //JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
