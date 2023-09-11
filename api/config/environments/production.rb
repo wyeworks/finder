7bdf5e234bbfe0c551cbc4be744a259fbf917d67
Juan Arias <juan.arias@cedarcode.com>
2023-09-11 18:45:32 -0300

    fix(config): Add default_url_options for production

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://finder-backend-qa.onrender.com' }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
