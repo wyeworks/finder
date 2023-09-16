b4d1c1ad5574fb11cfac1edb15caa9dbc4ac1874
marcelog@qualabs.com <mgodoydeolivera@gmail.com>
2023-09-16 13:36:17 -0300

    add tests

--- client/src/components/common/Alert.tsx
+++ client/src/components/common/Alert.tsx
@@ -2,6 +2,7 @@ import strings from '@/locales/strings.json';
 
 type AlertProps = {
   isVisible: boolean;
+  id?: string;
   errorMessage?: string;
   title?: string;
   type?: string;
@@ -12,10 +13,12 @@ export default function Alert({
   title = strings.common.error.defaultError,
   errorMessage = strings.common.error.unexpectedError,
   type = 'alert',
+  id = 'alert',
 }: AlertProps) {
   if (isVisible) {
     return (
       <div
+        data-testid={id}
         className='mt-2 h-20 h-fit content-center overflow-auto border-l-4 border-red-500 bg-red-100 p-3 text-sm text-red-600'
         role={type}
       >
