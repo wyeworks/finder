58b59b0bf292ec9f9f3918fd5827cde372d026d5
Cocofredo <3953441@gmail.com>
2023-09-18 19:34:12 -0300

    inpt change

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -1,3 +1,7 @@
+// import EyeClosedIcon from '@/assets/Icons/EyeClosedIcon';
+// import EyeIcon from '@/assets/Icons/EyeIcon';
+import { useState } from 'react';
+
 type InputParams = {
   type: string;
   id: string;
@@ -12,6 +16,8 @@ type InputParams = {
   // eslint-disable-next-line no-unused-vars
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   Icon?: React.ReactNode;
+  maxWidth?: boolean;
+  className?: string;
 };
 
 export default function Input({
@@ -26,34 +32,57 @@ export default function Input({
   value,
   onChange,
   Icon,
+  maxWidth = true,
+  className = '',
 }: InputParams) {
-  return (
-    <div className='w-full max-w-sm justify-center'>
-      {label && (
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  function renderLabel() {
+    if (label) {
+      return (
         <label
           htmlFor={id}
           className='block text-sm font-medium leading-6 text-gray-900'
         >
           {label}
         </label>
-      )}
-      <div className='relative -mb-3'>
+      );
+    }
+  }
+
+  return (
+    <div className={`${maxWidth && 'max-w-sm'} justify-center`}>
+      {renderLabel()}
+      <div className={`relative -mb-3 ${className}`}>
         {Icon && (
           <span className='pointer-events-none absolute inset-y-0 left-0 mt-2 flex h-fit items-center pl-3'>
             {Icon}
           </span>
         )}
+
+        {!Icon && type === 'password' && (
+          <VisibilityToggleButton
+            isVisible={isVisible}
+            toggleVisibility={toggleVisibility}
+          />
+        )}
+
         <input
           id={id}
+          data-testid={id}
           name={name}
-          type={type}
+          type={GetType(type, isVisible)}
           autoComplete={type}
           placeholder={placeholder}
           required={required}
           value={value}
           onChange={onChange}
           className={`peer block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 ${
-            Icon && 'pl-10'
+            (Icon || type === 'password') && 'pl-10'
           }`}
         />
         {touched && (
@@ -64,4 +93,33 @@ export default function Input({
       </div>
     </div>
   );
+}
+
+type VisibilityToggleButtonProps = {
+  isVisible: boolean;
+  toggleVisibility: () => void;
+};
+
+function VisibilityToggleButton({
+  isVisible,
+  toggleVisibility,
+}: VisibilityToggleButtonProps) {
+  return (
+    <button
+      type='button'
+      data-testid='visibility-toggle-button'
+      className='absolute inset-y-0 left-0 mt-2 flex h-fit items-center pl-3'
+      onClick={toggleVisibility}
+    >
+      {isVisible
+        ? // <EyeClosedIcon className='h-5 w-5 text-gray-400' />
+          'das'
+        : // <EyeIcon className='h-5 w-5 text-gray-400' />
+          'dasa'}
+    </button>
+  );
+}
+
+function GetType(type: string, isVisible: boolean) {
+  return type === 'password' ? (isVisible ? 'text' : 'password') : type;
 }
