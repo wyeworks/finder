09e561938b16e031797662e248d6d3688e60f95d
Cocofredo <3953441@gmail.com>
2023-09-19 10:28:29 -0300

    fixing test config profile

--- client/src/app/protected/configprofile/FormPersonalInfo.test.tsx
+++ client/src/app/protected/configprofile/FormPersonalInfo.test.tsx
@@ -1,4 +1,10 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@/__mocks__/next/router';
 import strings from '@/locales/strings.json';
@@ -26,9 +32,16 @@ describe('Form Personal Info Component', () => {
       )
     );
 
-    await waitFor(() => {
+    const inputElement = screen.getByLabelText(
+      strings.configProfile.forms.personalInfo.nameInput.label
+    );
+
+    // Use fireEvent to change the input value to an empty string
+    fireEvent.change(inputElement, { target: { value: '' } });
+
+    await waitFor(async () => {
       expect(
-        screen.queryByText('Por favor rellene todos los campos correctamente')
+        await screen.findByText(strings.common.error.completeFields)
       ).toBeInTheDocument();
     });
   });
@@ -53,15 +66,53 @@ describe('Form Personal Info Component', () => {
     userEvent.paste('Test Biography');
 
     // Submit the form
-    userEvent.click(
-      screen.getByText(
-        strings.configProfile.forms.personalInfo.submitButton.text
-      )
-    );
+    await act(async () => {
+      userEvent.click(
+        screen.getByText(
+          strings.configProfile.forms.personalInfo.submitButton.text
+        )
+      );
+    });
 
     // Wait for the fetch to be called
-    await waitFor(() => {
+    await waitFor(async () => {
       expect(fetch).toHaveBeenCalledWith('/api/signup', expect.anything());
+    });
+  });
+
+  it('should show success message when make a successful API call when form is submitted with valid data', async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({ ok: true }); // Mock a successful fetch call
+
+    render(<FormPersonalInfo user={user} />);
+
+    // Fill the form
+    screen
+      .getByLabelText(strings.configProfile.forms.personalInfo.nameInput.label)
+      .focus();
+    userEvent.paste('John Doe');
+    screen
+      .getByLabelText(
+        strings.configProfile.forms.personalInfo.birthdateInput.label
+      )
+      .focus();
+    userEvent.paste('2023-02-03');
+    screen.getByTestId('biography').focus();
+    userEvent.paste('Test Biography');
+
+    // Submit the form
+    await act(async () => {
+      userEvent.click(
+        screen.getByText(
+          strings.configProfile.forms.personalInfo.submitButton.text
+        )
+      );
+    });
+
+    // Wait for the succcess message to appear
+    await waitFor(async () => {
+      expect(
+        await screen.findByText(strings.common.success.changeSuccess)
+      ).toBeInTheDocument();
     });
   });
 
@@ -87,16 +138,18 @@ describe('Form Personal Info Component', () => {
     userEvent.paste('Test Biography');
 
     // Submit the form
-    userEvent.click(
-      screen.getByText(
-        strings.configProfile.forms.personalInfo.submitButton.text
-      )
-    );
+    await act(async () => {
+      userEvent.click(
+        screen.getByText(
+          strings.configProfile.forms.personalInfo.submitButton.text
+        )
+      );
+    });
 
     // Wait for the error message to appear
-    await waitFor(() => {
+    await waitFor(async () => {
       expect(
-        screen.queryByText('Ocurrio un error inesperado, intenta de nuevo')
+        await screen.findByText(strings.common.error.unexpectedError)
       ).toBeInTheDocument();
     });
   });
