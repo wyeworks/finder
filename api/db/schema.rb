2d0b56d06923c42e917ff635507a957293ceef61
Juan Arias <juan.arias@cedarcode.com>
2023-09-19 18:04:42 -0300

    feat(db/schema): Add `careers` table

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_17_201053) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_201036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "careers", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "code", null: false
+    t.string "approved_on"
+    t.integer "years"
+    t.integer "credits"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_careers_on_code", unique: true
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name", null: false
