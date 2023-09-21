e9dd40e0e1f084e85f9c706c3f2c0e3f39e76e03
Juan Arias <juan.arias@cedarcode.com>
2023-09-21 20:39:00 -0300

    chore(models): Remove associations between subjects and careers
      * Update factory and specs.

--- api/app/models/subject.rb
+++ api/app/models/subject.rb
@@ -1,7 +1,6 @@
 class Subject < ApplicationRecord
   # Associations
   has_many :groups, dependent: :nullify
-  has_and_belongs_to_many :careers
 
   # Validations
   validates :name, :credits, :code, presence: true
