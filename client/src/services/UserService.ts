ed3e8cc732e49830cdcaffec2b83ee3a6e0aa0d0
Frodo2412 <brunolemus553@gmail.com>
2023-09-24 16:31:55 -0300

    feat: hooked up to the BE

--- client/src/services/UserService.ts
+++ client/src/services/UserService.ts
@@ -1,16 +1,29 @@
 import { User } from '@/types/User';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/auth';
 
 export class UserService {
   public static async getUser(id: string): Promise<User> {
+    const session = await getServerSession(authOptions);
     const RAILS_API_URL = process.env.RAILS_API_URL;
 
     if (!RAILS_API_URL) {
       throw new Error('RAILS_API_URL is not defined');
     }
 
+    if (!session?.user.accessToken) {
+      throw new Error('Access token is not defined');
+    }
+
     const URL = process.env.RAILS_API_URL + '/users/' + id;
 
-    const res = await fetch(URL);
+    const res = await fetch(URL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: session?.user.accessToken,
+      },
+    });
     let json: any = await res.json();
     return (await json) as User;
   }
