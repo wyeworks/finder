1bd6e94a926b8e8ade712cb3a1c0a97b4984b67e
Cocofredo <3953441@gmail.com>
2023-10-04 19:08:38 -0300

    downsizing paddding

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import WyeWorksLogo from '@/assets/Icons/WyeWorksLogo';
 
 export default function Footer() {
   return (
-    <footer className='border-t-2 border-gray-200 bg-white p-4'>
+    <footer className='border-t-2 border-gray-200 bg-white p-2'>
       <div>
         <div className='mx-5 flex items-center justify-center'>
           <span className='text-black'>Powered by</span>
