65c8fdb2fc1d909ed0649cd6feae5d38693e2fca
Santiago <santiagomintegui@gmail.com>
2023-10-07 19:05:08 -0300

    delete hardcoded code

--- client/src/app/(protected)/groups/[id]/Sections.tsx
+++ client/src/app/(protected)/groups/[id]/Sections.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import PlayIcon from '@/assets/Icons/PlayIcon';
 import FolderIcon from '@/assets/Icons/FolderIcon';
 import ChatIcon from '@/assets/Icons/ChatIcon';
@@ -6,10 +5,8 @@ import GroupSizeIcon from '@/assets/Icons/GroupSizeIcon';
 import TimePreferences from './Content/TimePreferences';
 import { StudyGroup } from '@/types/StudyGroup';
 import Members from './Content/Members';
-import { Member } from '@/types/Member';
 import UserPlusIcon from '@/assets/Icons/UserPlusIcon';
 import RequestJoinGroup from './Content/RequestJoinGroup';
-// import { GroupService } from '@/services/GroupService';
 
 export type Section = {
   name: string;
@@ -17,61 +14,6 @@ export type Section = {
   // eslint-disable-next-line no-unused-vars
   content: (group: StudyGroup) => JSX.Element;
 };
-
-export const exampleUsers: Member[] = [
-  {
-    name: 'Juan Pérez',
-    email: 'juan@example.com',
-    role: 'participant',
-  },
-  {
-    name: 'María González',
-    email: 'maria@example.com',
-    role: 'admin',
-  },
-  {
-    name: 'Luis Rodríguez',
-    email: 'luis@example.com',
-    role: 'participant',
-  },
-  {
-    name: 'Ana López',
-    email: 'ana@exampleeeeeelargeeeeeeee.com',
-    role: 'admin',
-  },
-  {
-    name: 'Carlos Martínez',
-    email: 'carlos@example.com',
-    role: 'participant',
-  },
-  {
-    name: 'Laura Sánchez',
-    email: 'laura@example.com',
-    role: 'participant',
-  },
-  {
-    name: 'Pedro Ramirez',
-    email: 'pedro@example.com',
-    role: 'participant',
-  },
-  {
-    name: 'Isabel Pérez',
-    email: 'isabel@example.com',
-    role: 'admin',
-  },
-  {
-    name: 'Antonio García',
-    email: 'antonio@example.com',
-    role: 'participant',
-  },
-  {
-    name: 'Carmen Torres',
-    email: 'carmen@example.com',
-    role: 'participant',
-  },
-];
-
-const isAdmin = true;
 
 const sections: Section[] = [
   {
@@ -94,15 +36,12 @@ const sections: Section[] = [
   {
     name: 'Miembros',
     icon: <GroupSizeIcon className='mr-2 h-5 w-5' />,
-    // eslint-disable-next-line no-unused-vars
-    content: (group: StudyGroup) => <Members />,
+    content: () => <Members />,
   },
-  // this section will render only for admins
-  isAdmin && {
+  {
     name: 'Solicitudes',
     icon: <UserPlusIcon className='mr-2 h-5 w-5' />,
-    // eslint-disable-next-line no-unused-vars
-    content: (group: StudyGroup) => <RequestJoinGroup />,
+    content: () => <RequestJoinGroup />,
   },
 ];
 
