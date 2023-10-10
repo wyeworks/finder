59b15210bb5361b4a50a92379f9b0c8eedd4c659
Frodo2412 <brunolemus553@gmail.com>
2023-10-09 22:03:51 -0300

    fix: aesthetics

--- client/src/components/common/SocialNetworkButton.tsx
+++ client/src/components/common/SocialNetworkButton.tsx
@@ -18,9 +18,9 @@ function SocialNetworkButton(props: SocialNetworkButtonProps) {
     <a
       data-testid={`${props.name}Button`}
       href={props.link}
-      className='h-16 w-16'
+      className='m-2 h-6 w-6'
     >
-      {props.icon}
+      <div className={'flex items-center justify-center'}>{props.icon}</div>
     </a>
   );
 }
@@ -30,7 +30,13 @@ function InstagramButton(props: { link: string }) {
     <SocialNetworkButton
       name={'Instagram'}
       link={props.link}
-      icon={<InstagramIcon className='h-full w-full fill-gray-600' />}
+      icon={
+        <InstagramIcon
+          className='h-full w-full fill-gray-600'
+          width={16}
+          height={16}
+        />
+      }
     />
   );
 }
@@ -40,7 +46,13 @@ function LinkedInButton(props: { link: string }) {
     <SocialNetworkButton
       name={'LinkedIn'}
       link={props.link}
-      icon={<LinkedInIcon className='h-full w-full fill-gray-600' />}
+      icon={
+        <LinkedInIcon
+          className='h-full w-full fill-gray-600'
+          width={16}
+          height={16}
+        />
+      }
     />
   );
 }
@@ -50,7 +62,13 @@ function TwitterButton(props: { link: string }) {
     <SocialNetworkButton
       name={'Twitter'}
       link={props.link}
-      icon={<TwitterIcon className='h-full w-full fill-gray-600' />}
+      icon={
+        <TwitterIcon
+          className='h-full w-full fill-gray-600'
+          width={16}
+          height={16}
+        />
+      }
     />
   );
 }
@@ -60,7 +78,13 @@ function DiscordButton(props: { link: string }) {
     <SocialNetworkButton
       name={'Discord'}
       link={props.link}
-      icon={<DiscordIcon className='h-full w-full fill-gray-600' />}
+      icon={
+        <DiscordIcon
+          className='h-full w-full fill-gray-600'
+          width={16}
+          height={16}
+        />
+      }
     />
   );
 }
@@ -70,7 +94,13 @@ function RedditButton(props: { link: string }) {
     <SocialNetworkButton
       name={'Reddit'}
       link={props.link}
-      icon={<RedditIcon className='h-full w-full fill-gray-600' />}
+      icon={
+        <RedditIcon
+          className='h-full w-full fill-gray-600'
+          width={16}
+          height={16}
+        />
+      }
     />
   );
 }
@@ -80,17 +110,26 @@ function FacebookButton(props: { link: string }) {
     <SocialNetworkButton
       name={'Facebook'}
       link={props.link}
-      icon={<FacebookIcon className='h-full w-full fill-gray-600' />}
+      icon={
+        <FacebookIcon
+          className='h-full w-full fill-gray-600'
+          width={16}
+          height={16}
+        />
+      }
     />
   );
 }
 
 function WhatsappButton(props: { number: string }) {
   return (
-    <div className='flex h-16 flex-row items-start justify-center'>
-      <WhatsappIcon className='h-16 w-16 fill-gray-600 lg:ml-2' />
-      <p className='text-4xl text-gray-600'>{props.number}</p>
-    </div>
+    <a
+      href={`https://api.whatsapp.com/send/?phone=598${props.number}`}
+      className='flex h-12 flex-row items-start justify-center p-2'
+    >
+      <WhatsappIcon className='fill-gray-600 lg:ml-2' width={16} height={16} />
+      <p className='ml-2 text-lg/6 text-gray-600'>{props.number}</p>
+    </a>
   );
 }
 
