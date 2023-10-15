446d9ae951b43b1b6a16957d4a131a227e9c07ce
Andres <christianconti95@gmail.com>
2023-10-15 06:55:45 -0300

    test fixing

--- api/spec/requests/subjects_controller_spec.rb
+++ api/spec/requests/subjects_controller_spec.rb
@@ -1,25 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe SubjectsController, type: :request do
+  let(:user) { create(:user) }
+  let(:headers) do
+    post user_session_path, params: { user: { email: user.email, password: user.password } }
+    { 'Authorization' => response.headers['Authorization'] }
+  end
+
   # Index
   describe 'GET /subjects' do
     let(:subjects) { create_list :subject, 2 }
 
     before do
-      get subjects_path(subjects)
+      get subjects_path(subjects), headers:
     end
 
-    it 'returns a successful response' do
-      expect(response).to be_successful
+    context 'when user is authenticated' do
+      it 'returns a successful response' do
+        expect(response).to be_successful
+      end
+
+      it 'returns JSON containing all subjects data' do
+        json_response = response.parsed_body
+
+        expect(json_response[0]['id']).to be_a(Integer)
+        expect(json_response[0]['name']).to be_a(String)
+        expect(json_response[0]['code']).to be_a(String)
+        expect(json_response[0]['credits']).to be_a(Integer)
+      end
     end
 
-    it 'returns JSON containing all subjects data' do
-      json_response = response.parsed_body
+    context 'when user is not authenticated' do
+      before do
+        get subjects_path(subjects)
+      end
 
-      expect(json_response[0]['id']).to be_a(Integer)
-      expect(json_response[0]['name']).to be_a(String)
-      expect(json_response[0]['code']).to be_a(String)
-      expect(json_response[0]['credits']).to be_a(Integer)
+      it 'returns http unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 
@@ -28,20 +46,32 @@ RSpec.describe SubjectsController, type: :request do
     let(:subject) { create :subject }
 
     before do
-      get subject_path(subject)
+      get subject_path(subject), headers:
     end
 
-    it 'returns a successful response' do
-      expect(response).to be_successful
+    context 'when user is authenticated' do
+      it 'returns a successful response' do
+        expect(response).to be_successful
+      end
+
+      it 'returns JSON containing subject data' do
+        json_response = response.parsed_body
+
+        expect(json_response['id']).to eq(subject.id)
+        expect(json_response['name']).to eq(subject.name)
+        expect(json_response['code']).to eq(subject.code)
+        expect(json_response['credits']).to eq(subject.credits)
+      end
     end
 
-    it 'returns JSON containing subject data' do
-      json_response = response.parsed_body
+    context 'when user is not authenticated' do
+      before do
+        get subjects_path(subject)
+      end
 
-      expect(json_response['id']).to eq(subject.id)
-      expect(json_response['name']).to eq(subject.name)
-      expect(json_response['code']).to eq(subject.code)
-      expect(json_response['credits']).to eq(subject.credits)
+      it 'returns http unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
--- api/spec/requests/users_controller_spec.rb
+++ api/spec/requests/users_controller_spec.rb
@@ -1,26 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
+  # General
+  let(:headers) do
+    post user_session_path, params: { user: { email: user.email, password: user.password } }
+    { 'Authorization' => response.headers['Authorization'] }
+  end
+
   # Show
   describe 'GET /users/:id' do
     let(:user) { create :user, :with_social_networks }
 
     before do
-      get user_path(user)
+      get user_path(user), headers:
     end
 
-    it 'returns a successful response' do
-      expect(response).to be_successful
+    context 'when user is authenticated' do
+      it 'returns a successful response' do
+        expect(response).to be_successful
+      end
+
+      it 'returns JSON containing user data' do
+        json_response = response.parsed_body
+
+        expect(json_response['id']).to eq(user.id)
+        expect(json_response['email']).to eq(user.email)
+        expect(json_response['name']).to eq(user.name)
+        expect(DateTime.parse(json_response['birth_date'])).to eq(user.birth_date)
+        expect(json_response['social_networks']).to eq(user.social_networks)
+      end
     end
 
-    it 'returns JSON containing user data' do
-      json_response = response.parsed_body
+    context 'when user is not authenticated' do
+      before do
+        get user_path(user)
+      end
 
-      expect(json_response['id']).to eq(user.id)
-      expect(json_response['email']).to eq(user.email)
-      expect(json_response['name']).to eq(user.name)
-      expect(DateTime.parse(json_response['birth_date'])).to eq(user.birth_date)
-      expect(json_response['social_networks']).to eq(user.social_networks)
+      it 'returns http unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 
@@ -173,22 +191,34 @@ RSpec.describe UsersController, type: :request do
     let(:user) { create :user, :with_careers }
 
     before do
-      get careers_user_path(user)
+      get careers_user_path(user), headers:
     end
 
-    it 'returns a successful response' do
-      expect(response).to be_successful
+    context 'when user is authenticated' do
+      it 'returns a successful response' do
+        expect(response).to be_successful
+      end
+
+      it "returns JSON containing user's careers data" do
+        json_response = response.parsed_body
+
+        expect(json_response[0]['id']).to be_a(Integer)
+        expect(json_response[0]['name']).to be_a(String)
+        expect(json_response[0]['code']).to be_a(String)
+        expect(json_response[0]['approved_on']).to be_a(String)
+        expect(json_response[0]['years']).to be_a(Integer)
+        expect(json_response[0]['credits']).to be_a(Integer)
+      end
     end
 
-    it "returns JSON containing user's careers data" do
-      json_response = response.parsed_body
+    context 'when user is not authenticated' do
+      before do
+        get careers_user_path(user)
+      end
 
-      expect(json_response[0]['id']).to be_a(Integer)
-      expect(json_response[0]['name']).to be_a(String)
-      expect(json_response[0]['code']).to be_a(String)
-      expect(json_response[0]['approved_on']).to be_a(String)
-      expect(json_response[0]['years']).to be_a(Integer)
-      expect(json_response[0]['credits']).to be_a(Integer)
+      it 'returns http unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 
@@ -197,20 +227,32 @@ RSpec.describe UsersController, type: :request do
     let(:user) { create :user, :with_subjects }
 
     before do
-      get subjects_user_path(user)
+      get subjects_user_path(user), headers:
     end
 
-    it 'returns a successful response' do
-      expect(response).to be_successful
+    context 'when user is authenticated' do
+      it 'returns a successful response' do
+        expect(response).to be_successful
+      end
+
+      it "returns JSON containing user's subjects data" do
+        json_response = response.parsed_body
+
+        expect(json_response[0]['id']).to be_a(Integer)
+        expect(json_response[0]['name']).to be_a(String)
+        expect(json_response[0]['code']).to be_a(String)
+        expect(json_response[0]['credits']).to be_a(Integer)
+      end
     end
 
-    it "returns JSON containing user's subjects data" do
-      json_response = response.parsed_body
+    context 'when user is not authenticated' do
+      before do
+        get subjects_user_path(user)
+      end
 
-      expect(json_response[0]['id']).to be_a(Integer)
-      expect(json_response[0]['name']).to be_a(String)
-      expect(json_response[0]['code']).to be_a(String)
-      expect(json_response[0]['credits']).to be_a(Integer)
+      it 'returns http unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
