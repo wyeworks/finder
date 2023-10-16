d65be955e16f0386a6ff367135253f550c0de948
Santiago López <santiagolopezfolle@gmail.com>
2023-10-15 21:09:44 -0300

    remove Add Icon on header

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -11,7 +11,6 @@ import MenuIcon from '@/assets/Icons/MenuIcon';
 import ArrowDownIcon from '@/assets/Icons/ArrowDownIcon';
 import UserIcon from '@/assets/Icons/UserIcon';
 import EditIcon from '@/assets/Icons/EditIcon';
-import AddIcon from '@/assets/Icons/AddIcon';
 import LogOutIcon from '@/assets/Icons/LogOutIcon';
 import strings from '@/locales/strings.json';
 import { User } from '@/types/User';
