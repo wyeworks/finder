2ebf756c3a5a938fda27929d78cc544682f025d5
Cocofredo <3953441@gmail.com>
2023-10-18 10:01:57 -0300

    leaving groups title

--- client/src/app/(protected)/groups/page.tsx
+++ client/src/app/(protected)/groups/page.tsx
@@ -15,9 +15,7 @@ export default async function Groups() {
   return (
     <div className='flex flex-col'>
       <div className='p-5 text-[#2B2D54] lg:border-b-2 lg:bg-primaryBlue lg:text-white'>
-        <h1 className='text-center text-4xl font-extrabold'>
-          ¡Bienvenido a Finder!
-        </h1>
+        <h1 className='text-center text-4xl font-extrabold'>Grupos</h1>
       </div>
       <View groups={groups} subjects={subjects} />
     </div>
