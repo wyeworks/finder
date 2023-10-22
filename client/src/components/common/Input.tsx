3dca05bcf3e2aabfd5e5c6412151cc01f7e01a09
Santiago López <santiagolopezfolle@gmail.com>
2023-10-22 16:36:19 -0300

    Focus en inputs

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -116,7 +116,7 @@ export default function Input({
           required={required}
           value={value}
           onChange={onChange}
-          className={`peer block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 ${
+          className={`focus:ring-primaryBlue peer block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6 ${
             (Icon || type === 'password') && 'pl-10'
           } ${classNameInput}`}
           min={minNumber}
