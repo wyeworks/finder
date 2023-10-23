975f373ff5a4089aa73da8d5c41d3b8f71fc6c5e
Andres <christianconti95@gmail.com>
2023-10-23 20:29:00 -0300

    destroy_associated_sessions

--- api/app/models/member.rb
+++ api/app/models/member.rb
@@ -8,7 +8,16 @@ class Member < ApplicationRecord
   # Validations
   validates :role, presence: true, inclusion: { in: %w[admin participant] }
 
+  # Before
+  before_destroy :destroy_associated_sessions
+
   def promote!
     update!(role: 'admin')
+  end
+
+  private
+  def destroy_associated_sessions
+    sessions_created_by_member = Session.where(creator_id: self.id)
+    sessions_created_by_member.destroy_all
   end
 end
