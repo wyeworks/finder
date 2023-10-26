f4e858c116a2704faceb508474ece61ba1525631
Frodo2412 <brunolemus553@gmail.com>
2023-10-26 06:35:17 -0300

    feat: Mobile button

--- client/src/app/(protected)/groups/page.tsx
+++ client/src/app/(protected)/groups/page.tsx
@@ -53,15 +53,15 @@ export default function Groups() {
   return (
     <div className='flex h-full flex-col'>
       <div className='flex w-full justify-center bg-primaryBlue md:flex'>
-        <p className='flex w-[98%] items-center justify-center border-t-2 border-gray-500 py-5 text-2xl text-white'>
-          <h1 className='grow text-center text-4xl font-extrabold'>Grupos</h1>
-          <Link href={'/groups/new'} className='absolute right-0'>
-            <Button
-              text='Crear grupo'
-              Icon={<PlusIcon className='h-5 w-5' />}
-              classNameWrapper='sm:p-4 w-max p-0 m-0'
-              spaceBetween={8}
-              className='rounded-m h-12 items-center border-2'
+        <p className='flex w-[98%] items-center justify-center border-t-2 border-gray-500 py-5 text-2xltext-white'>
+        <h1 className='grow text-center text-4xl font-extrabold'>Grupos</h1>
+        <Link href={'/groups/new'} className='absolute right-0 mr-5'>
+          <Button
+            text='Crear grupo'
+            Icon={<PlusIcon className='h-5 w-5 md:mr-2' />}
+              classNameWrapper='h-12 w-12 md:w-fit p-0 m-0 rounded-full md:rounded-md  overflow-hidden'
+              className='h-12 w-12 items-center md:w-fit md:border-2'
+            hideTextOnMobile={true}
             />
           </Link>
         </p>
