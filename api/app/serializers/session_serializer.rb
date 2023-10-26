c3e7d0cd808192ed677133c676a1ad9fc59db735
Andres <christianconti95@gmail.com>
2023-10-26 19:44:08 -0300

    add member name to session attendence

--- api/app/serializers/session_serializer.rb
+++ api/app/serializers/session_serializer.rb
@@ -8,6 +8,19 @@ class SessionSerializer
              :meeting_link,
              :start_time,
              :end_time,
-             :group_id,
-             :attendances
+             :group_id
+
+  attribute :attendances do |session|
+    session.attendances.map do |attendance|
+      {
+        id: attendance.id,
+        session_id: attendance.session_id,
+        status: attendance.status,
+        created_at: attendance.created_at,
+        updated_at: attendance.updated_at,
+        member_id: attendance.member_id,
+        member_name: attendance.member.user.name
+      }
+    end
+  end
 end
