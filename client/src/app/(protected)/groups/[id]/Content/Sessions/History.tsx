dbe00908ab1e1a8a73b03cc8718003e362b03a0a
santi <santiagomintegui@gmail.com>
2023-10-27 20:24:39 -0300

    update test

--- client/src/app/(protected)/groups/[id]/Content/Sessions/History.tsx
+++ client/src/app/(protected)/groups/[id]/Content/Sessions/History.tsx
@@ -1,45 +1,6 @@
 import { Session } from '@/types/Session';
 import CardSession from './CardSession';
 
-export const exampleSession = {
-  sessions: {
-    id: 2,
-    name: 'Parcial bdd',
-    description: null,
-    location: 'fing',
-    meeting_link: 'linkmeet.com',
-    start_time: '2023-12-10T19:00:00.000Z',
-    end_time: '2023-12-11T19:00:00.000Z',
-    group_id: 1,
-    attendances: [
-      {
-        id: 1,
-        session_id: 2,
-        status: 'pending',
-        created_at: '2023-10-19T14:51:53.393Z',
-        updated_at: '2023-10-19T14:51:53.393Z',
-        member_id: 1,
-      },
-      {
-        id: 2,
-        session_id: 2,
-        status: 'pending',
-        created_at: '2023-10-19T14:51:53.405Z',
-        updated_at: '2023-10-19T14:51:53.405Z',
-        member_id: 44,
-      },
-      {
-        id: 3,
-        session_id: 2,
-        status: 'pending',
-        created_at: '2023-10-19T14:51:53.410Z',
-        updated_at: '2023-10-19T14:51:53.410Z',
-        member_id: 45,
-      },
-    ],
-  },
-};
-
 type HistoryProps = {
   sessions: Session[];
   // eslint-disable-next-line no-unused-vars
