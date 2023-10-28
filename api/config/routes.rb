51ea8d2f2448d9092e84adf2675d6dec1b2390b9
nodescanses <ivngtz11@gmail.com>
2023-10-28 19:27:17 -0300

    Added endpoint and mail to reset password

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   devise_for :users, path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    registration: 'signup'
+    registration: 'signup',
+    password: 'forgot_password'
   },
   controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    confirmations: 'users/confirmations'
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords'
   }
 
   resources :careers, only: :index
