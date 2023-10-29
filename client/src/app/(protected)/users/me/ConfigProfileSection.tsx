8c6bfec6693ee6b34f590f83fbaed6abbafc072b
Cocofredo <3953441@gmail.com>
2023-10-28 21:25:06 -0300

    adjusting form and id view of group

--- client/src/app/(protected)/users/me/ConfigProfileSection.tsx
+++ client/src/app/(protected)/users/me/ConfigProfileSection.tsx
@@ -34,7 +34,7 @@ export function ConfigProfileSection({
         {sectionTitle}
       </p>
       <form
-        className='grid grid-rows-personal-info-form gap-5 rounded-lg border border-gray-200 bg-white pl-7 pr-7 pt-2'
+        className='grid gap-5 rounded-lg border border-gray-200 bg-white pl-7 pr-7 pt-2'
         onSubmit={handleSubmit}
         noValidate
       >
