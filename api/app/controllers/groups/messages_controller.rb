4f93522c05fe8ffac87f89eece6d0e7934ef4bfd
Juan Arias <juan.arias@cedarcode.com>
2023-11-02 19:50:02 -0300

    test(messages): Add missing tests

--- api/app/controllers/groups/messages_controller.rb
+++ api/app/controllers/groups/messages_controller.rb
@@ -22,23 +22,30 @@ module Groups
         )
       )
       if @message.save
+        Rails.logger.info "Message was successfully created with params: '#{message_params}'"
+
         render json: MessageSerializer.new(@message).serializable_hash[:data][:attributes], status: :created
       else
-        Rails.logger.error "Failed to create Message: #{@message.errors.full_messages.join(', ')}"
-        render json: @message.errors, status: :unprocessable_entity
+        Rails.logger.info "Message has the following validation errors: #{@message.errors.full_messages}"
+
+        render json: {
+          message: 'El mensaje no pudo ser creado correctamente',
+          errors: @message.errors.messages
+        }, status: :unprocessable_entity
       end
     end
 
     def update
       if @message.update(message_params)
+        Rails.logger.info "Message was successfully updated with params: '#{message_params}'"
+
         render json: MessageSerializer.new(@message).serializable_hash[:data][:attributes], status: :ok
       else
-        Rails.logger.error 'Failed to update Message with ID ' \
-                           "##{@message.id}: #{@message.errors.full_messages.join(', ')}"
+        Rails.logger.info "Message has the following validation errors: #{@message.errors.full_messages}"
+
         render json: {
-          errors: {
-            message: @message.errors.full_messages
-          }
+          message: 'El mensaje no pudo ser actualizado correctamente',
+          errors: @message.errors.messages
         }, status: :unprocessable_entity
       end
     end
@@ -46,9 +53,11 @@ module Groups
     def destroy
       if @message.destroy
         Rails.logger.info "Message with ID ##{@message.id} was successfully destroyed."
+
         render json: { success: 'El mensaje fue eliminado exitosamente.' }, status: :ok
       else
-        Rails.logger.error "Failed to destroy Message with ID ##{@message.id}."
+        Rails.logger.info "Message has the following validation errors: #{@message.errors.full_messages}"
+
         render json: {
           errors: {
             message: ["No se pudo eliminar el mensaje con el ID ##{@message.id}"]
@@ -78,13 +87,13 @@ module Groups
     def ensure_member_access
       return if @group.users.include?(current_user)
 
-      render json: { error: 'Not Authorized' }, status: :forbidden
+      render json: { message: 'No estás autorizado para realizar esta acción' }, status: :unauthorized
     end
 
     def ensure_correct_user_or_admin
       return if @message.user == current_user || @group.admin?(current_user)
 
-      render json: { error: 'Not Authorized' }, status: :forbidden
+      render json: { message: 'No estás autorizado para realizar esta acción' }, status: :unauthorized
     end
 
     def message_params
--- api/spec/requests/groups/messages_controller_spec.rb
+++ api/spec/requests/groups/messages_controller_spec.rb
@@ -1,103 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe 'Groups::MessagesController', type: :request do
+  # General
   let(:user) { create :user }
   let(:group) { create :group }
-  let(:message) { create :message, user:, group: }
-
   let(:headers) do
     post user_session_path, params: { user: { email: user.email, password: user.password } }
     { 'Authorization' => response.headers['Authorization'] }
   end
 
-  # Create
-  describe 'POST /groups/:group_id/messages' do
-    context 'when user is authenticated' do
-      before { headers }
-
-      context 'when user is a member of the group' do
-        before do
-          group.members.create(user:, role: 'participant')
-          post group_messages_path(group), params: { message: { content: 'Test message content' } }, headers:
-        end
-
-        it 'creates a new message successfully' do
-          expect(response).to have_http_status(:created)
-          expect(Message.count).to eq(1)
-        end
-
-        it 'assigns the logged-in user as the message creator' do
-          expect(Message.first.user).to eq(user)
-        end
-      end
-
-      context 'when user is not a member of the group' do
-        before do
-          post group_messages_path(group), params: { message: { content: 'Test message content' } }, headers:
-        end
-
-        it 'does not create a new message' do
-          expect(Message.count).to eq(0)
-        end
-
-        it 'returns forbidden status' do
-          expect(response).to have_http_status(:forbidden)
-        end
-      end
-    end
-
-    context 'when user is authenticated and message creation fails' do
-      before do
-        group.members.create(user:, role: 'participant')
-        headers
-        post group_messages_path(group), params: { message: { content: '' } }, headers:
-      end
-
-      it 'does not create a new message' do
-        expect(Message.count).to eq(0)
-      end
-
-      it 'returns unprocessable entity status' do
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-
-      it 'returns the error messages' do
-        error_response = response.parsed_body
-        expect(error_response).to have_key('content')
-      end
-    end
-
-    context 'when user is not authenticated' do
-      before do
-        post group_messages_path(group), params: { message: { content: 'Test message content' } }
-      end
-
-      it 'does not create a new message' do
-        expect(Message.count).to eq(0)
-      end
-
-      it 'returns http unauthorized' do
-        expect(response).to have_http_status(:unauthorized)
-      end
-    end
-  end
-
   # Index
   describe 'GET /groups/:group_id/messages' do
-    let!(:messages) { create_list(:message, 5, user:, group:) } # Create 5 messages
+    context 'when group does not exist' do
+      before do
+        get group_messages_path(group_id: -1), headers:
+      end
+
+      it 'returns not found status' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'returns an error message' do
+        expect(response.parsed_body['errors']['group'])
+          .to include('No se pudo encontrar el grupo con el ID #-1')
+      end
+    end
 
     context 'when user is authenticated' do
-      before { headers }
+      let(:messages) { create_list(:message, 5, user:, group:) }
 
       context 'when user is a member of the group' do
         before do
-          group.members.create(user:, role: 'participant')
-          get group_messages_path(group), headers:
+          create :member, group:, user:, role: 'participant'
+
+          get group_messages_path(group, messages), headers:
         end
 
-        it 'returns a list of messages' do
-          expect(response).to have_http_status(:ok)
-          expect(response.parsed_body.size).to eq(5) # por tirar
+        it 'returns a successful response' do
+          expect(response).to be_successful
+        end
+
+        it 'returns JSON containing all messages from the group' do
+          json_response = response.parsed_body
+
+          expect(json_response[0]['id']).to be_a(Integer)
+          expect(json_response[0]['content']).to be_a(String)
+          expect(json_response[0]['hour']).to be_a(String)
+          expect(json_response[0]['user_id']).to eq(user.id)
+          expect(json_response[0]['group_id']).to eq(group.id)
+          expect(json_response[0]['user_name']).to eq(user.name)
         end
 
         it 'returns the correct message content' do
@@ -113,8 +64,8 @@ RSpec.describe 'Groups::MessagesController', type: :request do
           get group_messages_path(group), headers:
         end
 
-        it 'does not return the messages' do
-          expect(response).to have_http_status(:forbidden)
+        it 'returns http unauthorized' do
+          expect(response).to have_http_status(:unauthorized)
         end
       end
     end
@@ -124,140 +75,219 @@ RSpec.describe 'Groups::MessagesController', type: :request do
         get group_messages_path(group)
       end
 
-      it 'does not return the messages' do
+      it 'returns http unauthorized' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  # Create
+  describe 'POST /groups/:group_id/messages' do
+    let(:message_params) { { content: 'Test message content' } }
+
+    context 'when user is authenticated' do
+      context 'when user is a member of the group' do
+        before do
+          create :member, group:, user:, role: 'participant'
+
+          post group_messages_path(group), params: { message: message_params }, headers:
+        end
+
+        it 'creates a new message successfully' do
+          expect(response).to have_http_status(:created)
+          expect(Message.count).to eq(1)
+        end
+
+        it 'assigns the logged-in user as the message creator' do
+          expect(Message.first.user).to eq(user)
+        end
+
+        it 'has the right content' do
+          expect(Message.first.content).to eq('Test message content')
+        end
+      end
+
+      context 'when user is not a member of the group' do
+        before do
+          post group_messages_path(group), params: { message: message_params }, headers:
+        end
+
+        it 'does not create a new message' do
+          expect(Message.count).to eq(0)
+        end
+
+        it 'returns http unauthorized' do
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+
+      context 'when there are validation errors' do
+        let(:message_params) { { content: '' } }
+
+        before do
+          create :member, group:, user:, role: 'participant'
+
+          post group_messages_path(group), params: { message: message_params }, headers:
+        end
+
+        it 'does not create a new message' do
+          expect(Message.count).to eq(0)
+        end
+
+        it 'returns unprocessable entity status' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'returns JSON containing error messages' do
+          json_response = response.parsed_body
+
+          expect(json_response['message']).to include('El mensaje no pudo ser creado correctamente')
+          expect(json_response['errors']['content']).to include('El contenido del mensaje no puede ser vacío')
+        end
+      end
+    end
+
+    context 'when user is not authenticated' do
+      before do
+        post group_messages_path(group), params: { message: message_params }
+      end
+
+      it 'does not create a new message' do
+        expect(Message.count).to eq(0)
+      end
+
+      it 'returns http unauthorized' do
         expect(response).to have_http_status(:unauthorized)
       end
     end
   end
 
   # Update
-  describe 'PUT /groups/:group_id/messages/:id' do
-    let(:new_content) { 'Updated message content' }
+  describe 'PATCH /groups/:group_id/messages/:id' do
+    let(:message_params) { { content: 'Updated message content' } }
 
-    context 'when user is authenticated' do
-      before { headers }
+    context 'when user is one of the group\'s admin' do
+      let(:message) { create :message, group: }
 
-      context 'when update fails' do
-        before do
-          group.members.create(user:, role: 'participant')
-          allow_any_instance_of(Message).to receive(:update).and_return(false)
-          put group_message_path(group, message), params: { message: { content: new_content } }, headers:
-        end
+      before do
+        create :member, group:, user:, role: 'admin'
 
-        it 'returns unprocessable entity status' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
+        patch group_message_path(group, message), params: { message: message_params }, headers:
+      end
 
-        it 'returns the error messages' do
-          expect(response.parsed_body['errors']).to have_key('message')
-        end
+      it 'updates the message successfully' do
+        expect(response).to have_http_status(:ok)
+        expect(message.reload.content).to eq('Updated message content')
       end
     end
 
-    context 'when user is not authenticated' do
+    context 'when user is the message creator' do
+      let(:message) { create :message, group:, user: }
+
       before do
-        put group_message_path(group, message), params: { message: { content: new_content } }
+        create :member, group:, user:, role: 'participant'
+
+        patch group_message_path(group, message), params: { message: message_params }, headers:
       end
 
-      it 'does not update the message' do
-        expect(message.reload.content).not_to eq(new_content)
+      it 'updates the message successfully' do
+        expect(response).to have_http_status(:ok)
+        expect(message.reload.content).to eq('Updated message content')
+      end
+    end
+
+    context 'when user is not the message creator nor a group\'s admin' do
+      let(:message) { create :message, group: }
+
+      before do
+        create :member, group:, user:, role: 'participant'
+
+        patch group_message_path(group, message), params: { message: message_params }, headers:
       end
 
       it 'returns http unauthorized' do
         expect(response).to have_http_status(:unauthorized)
       end
-    end
-  end
 
-  # set_gorup
-  describe 'GET /groups/:group_id/messages' do
-    context 'when group does not exist' do
+      it 'does not update the message' do
+        expect(message.reload.content).not_to eq('Updated message content')
+      end
+
+      it 'returns JSON containing error message' do
+        json_response = response.parsed_body
+
+        expect(json_response['message']).to include('No estás autorizado para realizar esta acción')
+      end
+    end
+
+    context 'when there are validation errors' do
+      let(:message_params) { { content: '' } }
+      let(:message) { create :message, group:, user: }
+
       before do
-        get group_messages_path(group_id: -1), headers:
-      end
+        create :member, group:, user:, role: 'participant'
 
-      it 'returns not found status' do
-        expect(response).to have_http_status(:not_found)
-      end
-
-      it 'returns an error message' do
-        expect(response.parsed_body['errors']['group']).to include('No se pudo encontrar el grupo con el ID #-1')
-      end
-    end
-  end
-
-  # ensure_correct_user_or_admin
-
-  describe 'PUT /groups/:group_id/messages/:id' do
-    let(:new_content) { 'Updated message content' } # Define new_content here
-
-    let(:other_user) { create :user }
-    let(:other_headers) do
-      post user_session_path, params: { user: { email: other_user.email, password: other_user.password } }
-      { 'Authorization' => response.headers['Authorization'] }
-    end
-
-    context 'when user is not the creator of the message nor an admin' do
-      before do
-        group.members.create(user: other_user, role: 'participant')
-        put group_message_path(group, message), params: { message: { content: new_content } }, headers: other_headers
+        patch group_message_path(group, message), params: { message: message_params }, headers:
       end
 
       it 'does not update the message' do
-        expect(response).to have_http_status(:forbidden)
+        expect(message.reload.content).not_to be_empty
+      end
+
+      it 'returns unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns JSON containing error messages' do
+        json_response = response.parsed_body
+
+        expect(json_response['message']).to include('El mensaje no pudo ser actualizado correctamente')
+        expect(json_response['errors']['content']).to include('El contenido del mensaje no puede ser vacío')
       end
     end
   end
 
   # Destroy
   describe 'DELETE /groups/:group_id/messages/:id' do
-    context 'when user is authenticated' do
-      before { headers }
+    context 'when user is a member of the group and is the creator of the message' do
+      let(:message) { create :message, group:, user: }
 
-      context 'when user is a member of the group and is the creator of the message' do
-        before do
-          group.members.create(user:, role: 'participant')
-          delete group_message_path(group, message), headers:
-        end
+      before do
+        create :member, group:, user:, role: 'participant'
 
-        it 'deletes the message successfully' do
-          expect(response).to have_http_status(:ok)
-          expect(Message.exists?(message.id)).to be_falsey
-        end
+        delete group_message_path(group, message), headers:
       end
 
-      context 'when destruction fails' do
-        before do
-          group.members.create(user:, role: 'participant')
-          allow_any_instance_of(Message).to receive(:destroy).and_return(false)
-          allow(Rails.logger).to receive(:error)
-          delete group_message_path(group, message), headers:
-        end
-
-        it 'logs an error' do
-          expect(Rails.logger).to have_received(:error).with("Failed to destroy Message with ID ##{message.id}.")
-        end
-
-        it 'returns unprocessable entity status' do
-          expect(response).to have_http_status(:unprocessable_entity)
-          expect(response.parsed_body['errors']['message']).to include(
-            "No se pudo eliminar el mensaje con el ID ##{message.id}"
-          )
-        end
+      it 'deletes the message successfully' do
+        expect(response).to have_http_status(:ok)
+        expect(Message.exists?(message.id)).to be_falsey
       end
     end
 
-    context 'when user is not authenticated' do
+    context 'when user destruction fails' do
+      let(:message) { create :message, group:, user: }
+
       before do
-        delete group_message_path(group, message)
+        create :member, group:, user:, role: 'participant'
+
+        allow_any_instance_of(Message).to receive(:destroy).and_return(false)
+
+        delete group_message_path(group, message), headers:
+      end
+
+      it 'returns unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'does not delete the message' do
         expect(Message.exists?(message.id)).to be_truthy
       end
 
-      it 'returns http unauthorized' do
-        expect(response).to have_http_status(:unauthorized)
+      it 'returns JSON containing error messages' do
+        json_response = response.parsed_body
+
+        expect(json_response['errors']['message'])
+          .to include("No se pudo eliminar el mensaje con el ID ##{message.id}")
       end
     end
   end
