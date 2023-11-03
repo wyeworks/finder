7a01ee9d79773ae9b98509ae2196e46a5946f199
Mr-T-Smyth <dylansmyth206@gmail.com>
2023-11-03 18:40:48 -0300

    feat(ConfigurarGrupo): comment

--- client/src/app/(protected)/groups/[id]/edit/EditGroupPropsSection.tsx
+++ client/src/app/(protected)/groups/[id]/edit/EditGroupPropsSection.tsx
@@ -17,37 +17,6 @@ import { Logger } from '@/services/Logger';
 import EditableTimePreferences from '@/components/common/EditableTimePreferences';
 import TextArea from '@/components/common/TextArea';
 
-//export enum TimeOfDay {
-//   Morning = 'Morning',
-//   Afternoon = 'Afternoon',
-//   Night = 'Night',
-//   NoPreferences = 'None',
-//   No = '',
-// }
-//
-// export type TimePreference = {
-//   Sunday?: TimeOfDay;
-//   Monday?: TimeOfDay;
-//   Tuesday?: TimeOfDay;
-//   Wednesday?: TimeOfDay;
-//   Thursday?: TimeOfDay;
-//   Friday?: TimeOfDay;
-//   Saturday?: TimeOfDay;
-// };
-//
-// export type StudyGroup = {
-//   id?: number;
-//   name: string;
-//   description?: string;
-//   subject_id: number;
-//   subject_name?: string;
-//   size?: number;
-//   time_preferences?: TimePreference;
-//   isLab?: boolean;
-//   banner?: string;
-//   user_ids?: number[];
-// };
-
 interface EditableGroupData {
   name: string;
   description: string;
