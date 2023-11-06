d42048e8156e7b6611e4a61a984a1f114891c52a
Santiago López <santiagolopezfolle@gmail.com>
2023-11-06 19:15:55 -0300

    Poppins en labels de Dropdowns

--- client/src/components/common/DropDown.tsx
+++ client/src/components/common/DropDown.tsx
@@ -43,7 +43,7 @@ export default function Dropdown({
       {label && (
         <label
           htmlFor={id}
-          className='block text-sm font-medium leading-6 text-gray-900'
+          className='block font-poppins text-sm font-medium leading-6 text-gray-900'
         >
           {label}
         </label>
