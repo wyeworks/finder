545b8353c9d3265b532298b99049e9ede56e00fe
Santiago López <santiagolopezfolle@gmail.com>
2023-11-06 11:54:02 -0300

    Ahora si

--- client/src/app/(protected)/groups/[id]/Content/Sessions/CreateSessionForm.tsx
+++ client/src/app/(protected)/groups/[id]/Content/Sessions/CreateSessionForm.tsx
@@ -55,11 +55,11 @@ export default function CreateSessionForm({
       onSubmit={handleSubmit}
       data-testid='create-sesion'
     >
-      <div className='mr-2 mt-2 h-5 w-5'></div>
+      <div className='mr-2 mt-2 h-5 w-5' />
       <input
         name='title'
         type='text'
-        className='peer h-fit w-full border-b border-gray-300 text-xl focus:border-gray-600 focus:outline-none'
+        className='peer mt-3 h-fit w-full border-b border-gray-300 text-xl focus:border-gray-600 focus:outline-none'
         placeholder={strings.createSession.form.placeholders.title}
         value={formData.title}
         onChange={handleChange}
