723347a116411e36770e1aa08cd1f157c1f1bb18
marcelog@qualabs.com <mgodoydeolivera@gmail.com>
2023-11-10 14:13:03 -0300

    add some sessions test

--- client/src/app/(protected)/groups/[id]/Content/Sessions/__test__/Sessions.test.tsx
+++ client/src/app/(protected)/groups/[id]/Content/Sessions/__test__/Sessions.test.tsx
@@ -2,6 +2,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 import Sessions from '../Sessions';
 import { SessionProvider } from 'next-auth/react';
 import userEvent from '@testing-library/user-event';
+import { SessionService } from '@/services/SessionsService';
 
 jest.mock('next/navigation', () => ({
   usePathname: jest.fn().mockReturnValue('groups/1'),
@@ -21,6 +22,10 @@ const renderSessions = (isMember: boolean = true) => {
 };
 
 describe('Session Component', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   test('render session correct', () => {
     renderSessions();
   });
@@ -65,21 +70,54 @@ describe('Session Component', () => {
     ).toBeInTheDocument();
   });
 
-  // test WIP
-  it.skip('should handle form submission error', async () => {
+  it('calls createSession on form submit with the correct data', () => {
+    // Setup mock implementation of createSession
+    SessionService.createSession = jest
+      .fn()
+      .mockResolvedValue('Session created');
+
     renderSessions();
 
     // Click in Create session button
     userEvent.click(screen.getByText('Crear sesión'));
 
-    // fill form to create session (Required fields: title, start and end time )
+    waitFor(() => {
+      // Enter data into form fields
+      userEvent.type(screen.getByLabelText(/Title/i), 'Study Session');
+      userEvent.type(
+        screen.getByLabelText(/Description/i),
+        'Description of the session'
+      );
+      userEvent.type(screen.getByLabelText(/Location/i), 'Room 101');
+      userEvent.type(screen.getByLabelText(/Meet Link/i), 'http://meet.link');
+      userEvent.type(screen.getByLabelText(/Start Time/i), '2023-11-03T14:00');
+      userEvent.type(screen.getByLabelText(/End Time/i), '2023-11-03T15:00');
+    });
 
-    // send form
-    await userEvent.click(screen.getByText('Crear'));
+    // Submit the form
+    userEvent.click(screen.getByRole('button', { name: /Crear sesión/i }));
 
-    // waiting to error
-    await waitFor(() => {
-      expect(screen.queryByText('Error')).toBeInTheDocument();
+    // Wait for the createSession function to be called
+    waitFor(() => expect(SessionService.createSession).toHaveBeenCalled());
+
+    waitFor(() => {
+      // Check if the createSession was called with the correct parameters
+      expect(SessionService.createSession).toHaveBeenCalledWith(
+        expect.objectContaining({
+          name: 'Study Session',
+          description: 'Description of the session',
+          location: 'Room 101',
+          meeting_link: 'http://meet.link',
+          start_time: '2023-11-03T14:00',
+          end_time: '2023-11-03T15:00',
+          group_id: 1,
+        }),
+        expect.any(String) // accessToken
+      );
+
+      expect(
+        screen.getByText(/¡Sesión creada con éxito!/i)
+      ).toBeInTheDocument();
     });
   });
 });
