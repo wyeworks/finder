0837b86134ece57260b479fef01cc6601b3ba70d
santi <santiagomintegui@gmail.com>
2023-11-11 21:08:55 -0300

    order sessions

--- client/src/utils/Filters.tsx
+++ client/src/utils/Filters.tsx
@@ -3,15 +3,27 @@ import { Session } from '@/types/Session';
 export function filterExpiredSessions(sessions?: Session[]) {
   if (!sessions) return [];
   const today = new Date();
-  return sessions.filter(
+  const expiredSessions = sessions.filter(
     (session) => new Date(session.end_time.replace('Z', '')) < today
   );
+  expiredSessions.sort(
+    (a, b) =>
+      new Date(b.start_time.replace('Z', '')).getTime() -
+      new Date(a.start_time.replace('Z', '')).getTime()
+  );
+  return expiredSessions;
 }
 
 export function filterNextSessions(sessions?: Session[]) {
   if (!sessions) return [];
   const today = new Date();
-  return sessions.filter(
+  const nextSessions = sessions.filter(
     (session) => new Date(session.end_time.replace('Z', '')) >= today
   );
+  nextSessions.sort(
+    (a: any, b: any) =>
+      new Date(b.start_time.replace('Z', '')).getTime() -
+      new Date(a.start_time.replace('Z', '')).getTime()
+  );
+  return nextSessions;
 }
