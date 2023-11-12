89745d0a3c121e42da5d7c598aa34b9cc68158c3
Andres <christianconti95@gmail.com>
2023-11-12 17:34:08 -0300

    seed task: group and session added

--- api/db/seeds/resources.rb
+++ api/db/seeds/resources.rb
@@ -47,7 +47,67 @@ user_three = User.create!(
 )
 
 # Groups
-# TODO: Create groups
+group_one = Group.create!(
+  name: 'Group One',
+  description: 'This is Group One\'s description',
+  size: 10,
+  time_preferences: {
+    'Friday' => 'None',
+    'Sunday' => 'Morning'
+  },
+  subject_id: 1
+)
+
+creator_member_one = group_one.members.create!(
+  user: user_one,
+  role: 'admin'
+)
+
+group_one.members.create!(
+  user: user_three,
+  role: 'participant'
+)
+
+group_two = Group.create!(
+  name: 'Group Two',
+  description: 'This is Group Two\'s description',
+  size: 8,
+  time_preferences: {
+    'Monday' => 'Morning',
+    'Wednesday' => 'Night'
+  },
+  subject_id: 2
+)
+
+creator_member_two = group_two.members.create!(
+  user: user_two,
+  role: 'admin'
+)
+
+group_two.members.create!(
+  user: user_three,
+  role: 'participant'
+)
 
 # Sessions
-# TODO: Create sessions
+Session.create!(
+  name: 'Session One',
+  description: 'This is Session One\'s description',
+  location: 'Location One',
+  meeting_link: 'https://meet.example.com/session1',
+  start_time: DateTime.now + 1.day,
+  end_time: DateTime.now + 1.day + 2.hours,
+  group_id: group_one.id,
+  creator: creator_member_one
+)
+
+Session.create!(
+  name: 'Session Two',
+  description: 'This is Session Two\'s description',
+  location: 'Location Two',
+  meeting_link: 'https://meet.example.com/session2',
+  start_time: DateTime.now + 2.days,
+  end_time: DateTime.now + 2.days + 2.5.hours,
+  group_id: group_two.id,
+  creator: creator_member_two
+)
